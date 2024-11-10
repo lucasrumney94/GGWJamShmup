f482a15f75af4abf5d6ec2654640405d6d942922
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2024-11-10 12:41:36 -0800

    fixed the resolution maybe

--- Main/main_menu.gd
+++ Main/main_menu.gd
@@ -2,9 +2,38 @@ extends Node
 
 @export var main_game: PackedScene
 
+@export var card_collection: PackedScene
+@export var options_menu: PackedScene
+@export var upgrade_menu: PackedScene
+
+@export var quit_scene: PackedScene
+
+var submenu: Node
+
 
 func _ready():
 	%StartButton.pressed.connect(on_start_pressed)
+	%DeckButton.pressed.connect(open_submenu.bind(card_collection, %DeckButton))
+	%UpgradeButton.pressed.connect(open_submenu.bind(upgrade_menu, %UpgradeButton))
+	%OptionsButton.pressed.connect(open_submenu.bind(options_menu, %OptionsButton))
+	%QuitButton.pressed.connect(open_submenu.bind(quit_scene, %QuitButton))
+
+
+func open_submenu(scene: PackedScene, button: Button = null):
+	if submenu != null:
+		submenu.queue_free()
+	submenu = scene.instantiate()
+	get_parent().add_child(submenu)
+	if submenu.has_signal("closing"):
+		submenu.closing.connect(focus_on_button.bind(button))
+
+
+func focus_on_button(button: Button):
+	if button == null:
+		%StartButton.grab_focus()
+		return
+	
+	button.grab_focus()
 
 
 func on_start_pressed():
